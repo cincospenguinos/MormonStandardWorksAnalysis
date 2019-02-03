ad6dbdc3a56a191b8de6410816005ce38df1fc64
Andre LaFleur <andre.lafleur@utah.edu>
2019-02-02 23:09:19 -0700

    Add versioning info setup

--- src/main/java/mormon/Main.java
+++ src/main/java/mormon/Main.java
@@ -4,16 +4,21 @@ import mormon.report.AnalysisReport;
 import mormon.analysis.SimpleNGramAnalysis;
 import mormon.model.AnnotatedText;
 import mormon.extractors.TextGatherer;
+import mormon.version.VersionInfo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Main class. Entry point for the application.
  */
 public class Main {
+
+    private static final String CURRENT_VERSION = "0.1.0";
+    private static final String VERSION_INFO_FILE_NAME = "analysis_version_info.json";
 
     private static final String TEXTS_DIR = "src/main/resources/texts/";
     private static final String OUTPUT_DIR = "output";
@@ -21,6 +26,8 @@ public class Main {
     public static void main(String[] args) {
         TextGatherer gatherer = new TextGatherer(new File(TEXTS_DIR).listFiles());
         gatherer.gatherTexts();
+
+        createVersionReportFile(gatherer);
 
         for (AnnotatedText mormonText : gatherer.getMormonTexts()) {
             for (AnnotatedText nonMormonText : gatherer.getNonMormonTexts()) {
@@ -35,33 +42,70 @@ public class Main {
         }
     }
 
+    /**
+     * Helper method. Outputs report file detailing information about the current version of this application.
+     *
+     * @param gatherer -
+     */
+    private static void createVersionReportFile(TextGatherer gatherer) {
+        VersionInfo info = new VersionInfo();
+
+        info.setVersion(CURRENT_VERSION);
+        info.addMormonBooks(
+                gatherer.getMormonTexts().stream()
+                        .map(AnnotatedText::getName)
+                        .collect(Collectors.toList()));
+        info.addNonMormonBooks(
+                gatherer.getNonMormonTexts().stream()
+                        .map(AnnotatedText::getName)
+                        .collect(Collectors.toList()));
+
+        String json = info.toJson();
+        writeJsonToFile(VERSION_INFO_FILE_NAME, json);
+    }
+
+    /**
+     * Helper method. Creates output files given a filename chunk (the first part of the file name) and the set of all
+     * the json reports we performed.
+     *
+     * @param filenameChunk -
+     * @param jsonReports -
+     */
     private static void outputFilesFor(String filenameChunk, Map<String, String> jsonReports) {
         ensureOutputDirCreated();
 
         for (Map.Entry<String, String> e : jsonReports.entrySet()) {
-            StringBuilder fullFileName = new StringBuilder();
-            fullFileName.append(filenameChunk);
-            fullFileName.append("_");
-            fullFileName.append(e.getKey());
-            fullFileName.append(".json");
-
-            try {
-                PrintWriter writer = new PrintWriter(new File(OUTPUT_DIR + "/" + fullFileName.toString()));
-                writer.print(e.getValue());
-                writer.flush();
-                writer.close();
-            } catch (FileNotFoundException e1) {
-                throw new RuntimeException("Could not find the file!");
-            }
+            String fullFileName = filenameChunk + "_" + e.getKey() + ".json";
+            writeJsonToFile(fullFileName, e.getValue());
         }
     }
 
+    /**
+     * Ensures that the output directory is created to put stuff out into.
+     */
     private static void ensureOutputDirCreated() {
         File outputDir = new File(OUTPUT_DIR);
         if (!outputDir.exists() || !outputDir.isDirectory()) {
             if (!outputDir.mkdir()) {
                 throw new RuntimeException("Could not make output dir!");
             }
+        }
+    }
+
+    /**
+     * Helper method. Does what it says on the tin.
+     * @param filename
+     * @param json
+     */
+    private static void writeJsonToFile(String filename, String json) {
+        String fullFileName = OUTPUT_DIR + "/" + filename;
+        try {
+            PrintWriter printWriter = new PrintWriter(new File(fullFileName));
+            printWriter.print(json);
+            printWriter.flush();
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Could not write version report file!");
         }
     }
 }
---
+++ src/main/java/mormon/version/VersionInfo.java
@@ -0,0 +1,42 @@
+package mormon.version;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * VersionInfoBuilder
+ *
+ * Class that builds information about the current version of this application. Helps indicate to the user of the
+ * website and data visualization if there was a new version released or not.
+ */
+public class VersionInfo {
+
+    private String version;
+    private List<String> mormonBooks;
+    private List<String> nonMormonBooks;
+
+    public VersionInfo() {
+        mormonBooks = new ArrayList<>();
+        nonMormonBooks = new ArrayList<>();
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void addMormonBooks(Collection<String> mormonBooks) {
+        this.mormonBooks.addAll(mormonBooks);
+    }
+
+    public void addNonMormonBooks(Collection<String> nonMormonBooks) {
+        this.nonMormonBooks.addAll(nonMormonBooks);
+    }
+
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+}
